980d94f372a5369dbb6d87e13046272e43a8db10
Johnstonstu <stu.w.johnston@gmail.com>
2023-04-30 21:05:26 -0700

    added reset button

--- main.py
+++ main.py
@@ -1,44 +1,84 @@
 import turtle
+import tkinter as tk
+import random
+
+# Draw a polygon with a given number of sides and side length
 
 
-def draw_hexagon(size):
-    for _ in range(6):
-        turtle.forward(size)
-        turtle.left(60)
+def draw_polygon(sides, length):
+    angle = 360 / sides
+    for _ in range(sides):
+        turtle.forward(length)
+        turtle.left(angle)
+
+# Draw a tessellation of (3, 6, 3, 6) pattern
 
 
-def hexagon_row(size, count):
-    for _ in range(count):
-        draw_hexagon(size)
-        turtle.forward(size)
-
-
-def hexagon_tessellation(size, rows, cols):
-    turtle.speed(0)
-    turtle.bgcolor("white")
-
+def tessellation_3_6_3_6(size, rows, cols):
     for row in range(rows):
-        turtle.penup()
-        turtle.goto(-size * cols / 2, size * 1.5 * row - size * rows / 2)
-        turtle.pendown()
+        for col in range(cols):
+            if (row + col) % 2 == 0:
+                draw_polygon(3, size)
+                turtle.forward(size)
+            else:
+                draw_polygon(6, size)
+                turtle.forward(size)
+        turtle.backward(cols * size)
+        turtle.left(60)
+        turtle.forward(size)
+        turtle.right(60)
 
-        # If it's an even row, draw one less hexagon
-        hexagon_count = cols - (row % 2)
-        hexagon_row(size, hexagon_count)
-
-        # If it's an odd row, start with a horizontal offset
-        if row % 2 == 1:
-            turtle.penup()
-            turtle.goto(-size * cols / 2 + size / 2, size *
-                        1.5 * (row + 1) - size * rows / 2)
-            turtle.pendown()
+# Draw a tessellation of (4, 8, 8) pattern
 
 
-# Customize the tessellation pattern
-size = 30
-rows = 10
-cols = 10
+def tessellation_4_8_8(size, rows, cols):
+    for row in range(rows):
+        for col in range(cols):
+            if (row + col) % 2 == 0:
+                draw_polygon(4, size)
+                turtle.forward(size)
+            else:
+                draw_polygon(8, size)
+                turtle.forward(size)
+        turtle.backward(cols * size)
+        turtle.left(90)
+        turtle.forward(size)
+        turtle.right(90)
 
-hexagon_tessellation(size, rows, cols)
 
-turtle.done()
+def random_tessellation():
+    turtle.clear()
+    turtle.speed(0)
+    turtle.penup()
+    turtle.goto(-200, 200)
+    turtle.pendown()
+
+    size = 30
+    rows = 12
+    cols = 12
+
+    tessellations = [tessellation_3_6_3_6, tessellation_4_8_8]
+    chosen_tessellation = random.choice(tessellations)
+    chosen_tessellation(size, rows, cols)
+
+
+def main():
+    # Set up the turtle graphics window
+    turtle_screen = turtle.Screen()
+    turtle_screen.title("Random Tessellation Generator")
+    turtle.speed(0)
+
+    # Set up the tkinter window and button
+    tk_root = tk.Tk()
+    tk_root.withdraw()
+
+    button = tk.Button(
+        tk_root, text="Generate Random Tessellation", command=random_tessellation)
+    button.pack()
+
+    # Show the tkinter window and start the main event loop
+    tk_root.deiconify()
+    tk_root.mainloop()
+
+
+main()
